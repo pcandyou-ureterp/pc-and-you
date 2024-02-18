d1e9c5547ba7b6f03ee072f8b81b485249551796
Stefan Sijbesma <stefansijbesma12@gmail.com>
2024-02-18 09:18:52 +0100

    Add navigation actions + make responsive

--- scripts/navigation-animation.js
+++ scripts/navigation-animation.js
@@ -1,4 +1,8 @@
 
 function onNavElementClick (location) {
-    console.log("Navigating to " + location + "...");
+    const yOffset = -40; 
+    const element = document.querySelector(`#section-${location}`);
+    const y = element.getBoundingClientRect().top + window.scrollY + yOffset;
+
+    window.scrollTo({top: y, behavior: 'smooth'});
 }
